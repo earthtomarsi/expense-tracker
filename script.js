1251578dcb6b979fca4ab23cfd1eed883785c313
Mariel <guerra.mld@gmail.com>
2026-03-30 21:28:08 +1100

    Add addExpense function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 let expenses = [];
+
+function addExpense() {
+    const name = document.getElementById("name").value;
+    const amount = Number(document.getElementById("amount").value);
+    const category = document.getElementById("category").value;
+  
+    if (!name || !amount) return;
+  
+    const expense = {
+      name: name,
+      amount: amount,
+      category: category
+    };
+  
+    expenses.push(expense);
+  
+    console.log(expenses);
+  }
